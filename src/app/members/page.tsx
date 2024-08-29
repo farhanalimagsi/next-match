6ed3b64406497f186f838c8c897fec5bca6ac24c
farhanalimagsi <farhanmagsi2015@gmail.com>
2024-08-29 23:47:27 +0300

    End of Section 2

---
+++ src/app/members/page.tsx
@@ -0,0 +1,11 @@
+import { Link } from '@nextui-org/react'
+import React from 'react'
+
+export default function MembersPage() {
+  return (
+    <div>
+        <h1 className='text-3xl'>This will be the members page</h1>
+        <Link href='/'>Go Back Home</Link>
+    </div>
+  )
+}
